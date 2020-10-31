853b093b32b4919712aa17c0994e88eae4de51c6
samdisk11 <thexkey@pootisnetwork.heliohost.org>
2020-10-30 19:14:12 -0500

    fixes for Gmod 13.10.2020 (Part 2)
    oops forgot to fix the loader LOL

--- lua/achievementsplus/achieve_init.lua
+++ lua/achievementsplus/achieve_init.lua
@@ -1,7 +1,7 @@
 include ("achievements.lua")
-include ("achievementnotifications.lua")
-include ("achievementswindow.lua")
+include ("achieve_notifications.lua")
+include ("achieve_window.lua")
 
 concommand.Add ("achievements_reload", function (_, _, _)
-	include ("achievementsplus/achievements_init.lua")
+	include ("achievementsplus/achieve_init.lua")
 end)
